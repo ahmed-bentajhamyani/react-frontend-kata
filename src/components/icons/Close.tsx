c154055f1d3edf8b7fb5a77102c1da7e025b63af
ahmed-bentajhamyani <ahmedbentaj710@gmail.com>
2024-04-01 16:16:25 +0000

    add cart icons

---
+++ src/components/icons/Close.tsx
@@ -0,0 +1,7 @@
+export default function Close() {
+    return (
+        <svg width={25} height={25} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 5L19 19M5 19L19 5" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
