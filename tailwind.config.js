c859b3d081bd15347ee0bde62977656852b1e92d
ahmed-bentajhamyani <ahmedbentaj710@gmail.com>
2024-04-01 12:25:42 +0000

    add app colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#088F8F",
+        secondary: "#EEE",
+        text: {
+          primary: "#111",
+        }
+      },
+    },
   },
   plugins: [],
 }
