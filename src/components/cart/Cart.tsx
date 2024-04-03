3edaec0f63b1953913afb28e2725cee5ec17b59c
Ahmed Bentaj Hamyani <ahmedbentaj710@gmail.com>
2024-04-03 09:29:39 +0000

    fix: cannot read properties of null (reading 'reduce') error

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -81,10 +81,13 @@ function Cart() {
                             {Intl.NumberFormat("en-US", {
                                 style: "currency",
                                 currency: "USD",
-                            }).format(cartProducts?.reduce((total: number, cartProduct: CartItem) => {
-                                const product = products?.find((a) => a.id === cartProduct.id);
-                                return total + (parseFloat(product?.price || '0')) * cartProduct.quantity;
-                            }, 0)
+                            }).format(cartProducts?.length > 0 ?
+                                cartProducts.reduce((total: number, cartProduct: CartItem) => {
+                                    const product = products?.find((a) => a.id === cartProduct.id);
+                                    return total + (parseFloat(product?.price || '0')) * cartProduct.quantity;
+                                }, 0)
+                                :
+                                0
                             )}
                         </span>
                     </div>
